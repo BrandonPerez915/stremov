89e778be60a9e203f14445f6e034ca4141ee4098
Brandon Pérez <brandonperez@MacBook-Pro-de-Brandon.local>
2026-05-02 17:50:07 -0600

    modified server.js to redirect routes to their respective router

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import { connectDB } from './config/db.js'
 import apiRouter from './routes/apiRouter.js'
+import viewsRouter from './routes/viewsRouter.js'
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 connectDB()
 
+app.use(cors())
 app.use(express.json())
+app.use('/views', express.static(path.join(__dirname, '../frontend/views')))
+app.use('/assets', express.static(path.join(__dirname, '../frontend/assets')))
+app.use('/components', express.static(path.join(__dirname, '../frontend/components')))
+app.use('/scripts', express.static(path.join(__dirname, '../frontend/scripts')))
+
 app.use('/api', apiRouter)
+app.use('/', viewsRouter)
 
 app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"))
